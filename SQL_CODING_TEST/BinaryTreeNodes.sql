89cc61cae0d2e13ec21bbae28c4b71e53de33c8a
gwcat0506 <gwcat0506@naver.com>
2022-06-24 16:53:04 +0900

    update 20220624

--- SQL_CODING_TEST/BinaryTreeNodes.sql
+++ SQL_CODING_TEST/BinaryTreeNodes.sql
@@ -1,3 +1,6 @@
+--  오답 !! 다시 풀어서 제출해야됨
+
+
 SELECT  N, CASE WHEN P IS NULL THEN 'Root' 
         WHEN N NOT IN (SElECT Distinct P FROM bst WHERE P IS NOT NULL) THEN 'Leaf'
                ELSE 'Inner' END
