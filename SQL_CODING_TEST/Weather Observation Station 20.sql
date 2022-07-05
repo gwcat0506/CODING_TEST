380ad7a7aca96e3b05e9f560f805af4c12408f12
gwcat0506 <gwcat0506@naver.com>
2022-07-05 23:41:39 +0900

    20220705

---
+++ SQL_CODING_TEST/Weather Observation Station 20.sql
@@ -0,0 +1,15 @@
+-- 정말 어려웠던 문제... 
+-- lat_n의 median을 소숫점 4번째 자리 수에서 반올림
+
+-- mysql에서는 median을 직접구해야 한다.....OMG!!
+SET @r = 0;
+SELECT ROUND(AVG(Lat_N), 4)
+FROM (SELECT (@r := @r + 1) AS r, Lat_N FROM Station ORDER BY Lat_N) Temp
+WHERE
+    r = (SELECT CEIL(COUNT(*) / 2) FROM Station) OR
+    r = (SELECT FLOOR((COUNT(*) / 2) + 1) FROM Station)
+
+-- mysql에서는 SET함수를 통해서 변수를 설정해줄 수 있다.
+-- 그리고 변수를 설정해줄 때 @를 통해서 설정해준다.
+
+-- 더 어려워서 설명을 추가해야할 듯..
