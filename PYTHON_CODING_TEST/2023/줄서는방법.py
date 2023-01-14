61f587ef969f2f6017409fc5918b6d5ca81d635f
username <gwcat0506@naver.com>
2023-01-15 00:01:49 +0900

    230115

--- PYTHON_CODING_TEST/2023/줄서는방법.py
+++ PYTHON_CODING_TEST/2023/줄서는방법.py
@@ -6,6 +6,8 @@ from itertools import permutations
 def solution(n, k):
 
     a = [i for i in range(1,n+1)]
-    per_list = permutations(a, n)
-    print(list(per_list))
-    # return answer
+    per_list = list(permutations(a, n))
+    
+#     정렬하기
+    per_list = sorted(per_list)
+    return per_list[k-1]
