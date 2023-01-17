edaa112864cfbf3dfde180d2420a47a1a0bf9101
username <gwcat0506@naver.com>
2023-01-18 02:42:35 +0900

    dp

--- PYTHON_CODING_TEST/2023/DP_2193.py
+++ PYTHON_CODING_TEST/2023/DP_2193.py
@@ -22,7 +22,9 @@ def fib(N):
         
         return A[N-1]
 print(fib(N))
-    
+
+# 아래처럼도 풀 수 있다.(참고)
+# https://yongku.tistory.com/entry/%EB%B0%B1%EC%A4%80-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EB%B0%B1%EC%A4%80-2193%EB%B2%88-%EC%9D%B4%EC%B9%9C%EC%88%98-%ED%8C%8C%EC%9D%B4%EC%8D%ACPython
     
 # 1,2 ->1
 # 3 -> 2
