256d7b120badfa247b020373e2771b3c3b7d4cd5
username <gwcat0506@naver.com>
2023-01-18 02:22:40 +0900

    230118

---
+++ PYTHON_CODING_TEST/2023/DP_11053.py
@@ -0,0 +1,23 @@
+# https://www.acmicpc.net/problem/11053
+# 가장 긴 증가하는 부분 수열
+
+# DP 문제이다.
+
+import sys
+
+N = int(sys.stdin.readline())
+N_list = list(map(int,sys.stdin.readline().split()))
+
+print(N)
+print(N_list)
+
+
+A = [0]*100
+def bubun(x):
+    
+    for i in range(len(N_list)):
+        if A[i]!=0:
+        
+    
+    return 
+    
---
+++ PYTHON_CODING_TEST/2023/DP_2193.py
@@ -0,0 +1,22 @@
+# https://www.acmicpc.net/problem/2193
+# 이친수
+
+# DP
+
+
+import sys
+
+N = int(sys.stdin.readline())
+
+lee = '10'
+
+if N<=2:
+    print(N)
+
+
+
+
+
+
+
+
