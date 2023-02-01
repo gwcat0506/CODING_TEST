0c4a213487790a34645e43911b1c901cc7c352c3
GounChoi <61350052+gwcat0506@users.noreply.github.com>
2023-02-02 00:57:23 +0900

    230201

--- PYTHON_CODING_TEST/2023/[2493] 탑.py
+++ PYTHON_CODING_TEST/2023/[2493] 탑.py
@@ -1,49 +1,24 @@
 import sys
 
-N = int(sys.stdin.readline())
-N_list = list(map(int,sys.stdin.readline().split()))
+input = sys.stdin.readline
+N = int(input())
+N_list = list(map(int,input().split()))
 
 st = []
-# pair로 push 
-st.append((0,N_list[0]))
+result = []
 
-result = [0]
-
-for i in range(1,N):
-      
-    if st[-1][1] < N_list[i] :
-        # 9 < 5
-        st.pop() 
-        if len(st)==0:
-            result.append(0) 
+for i in range(0,N):
+    while st:
+        if N_list[st[-1]] >= N_list[i] :
+            # 9 < 5
+            break
         else:
-            # print(st[-1])
-            result.append(N_list.index(st[-1][1])+1)
+            st.pop()   
+    if st:
+        result.append(st[-1]+1)
     else:
-        # print(st[-1])
-        result.append(N_list.index(st[-1][1])+1)
-    st.append((i+1,N_list[i]))
-    # print(st)
+        result.append(0)
+    st.append(i)
+
+# print(st)
 print(*result)
-        
-
-# for i in range(1,N):
-#     # 입력받은 값이 현재 높이 값 보다 낮으면 pop
-#     if st[-1] < N_list[i] :
-#         5 < 7
-#         st.pop()
-#         # pop 후 stack 비어있을 경우 
-#         if len(st)==0:
-#             idx.append(0)
-#         else:
-#             idx.append(i)
-#     else: 
-#         idx.append(i)
-        
-    
-#     st.append(N_list[i])
-    
-    
-# print(*idx)
-        
-
