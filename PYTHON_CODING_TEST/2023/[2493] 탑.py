eba92fc1edf462b0a11c1d67917f59f4c1a24e5a
GounChoi <61350052+gwcat0506@users.noreply.github.com>
2023-02-01 23:59:18 +0900

    230201

--- PYTHON_CODING_TEST/2023/[2493] 탑.py
+++ PYTHON_CODING_TEST/2023/[2493] 탑.py
@@ -3,15 +3,47 @@ import sys
 N = int(sys.stdin.readline())
 N_list = list(map(int,sys.stdin.readline().split()))
 
+st = []
+# pair로 push 
+st.append((0,N_list[0]))
 
-# 0,1,2,3,4
-for i in range(len(N_list)):
-    # 4,3,2,1,0
-    print(N_list[N-i-1])
-    # 4,3,2,1
-    for j in range(i-1):
-        print(N_list[N-i-1],N_list[j-1])
+result = [0]
+
+for i in range(1,N):
+      
+    if st[-1][1] < N_list[i] :
+        # 9 < 5
+        st.pop() 
+        if len(st)==0:
+            result.append(0) 
+        else:
+            # print(st[-1])
+            result.append(N_list.index(st[-1][1])+1)
+    else:
+        # print(st[-1])
+        result.append(N_list.index(st[-1][1])+1)
+    st.append((i+1,N_list[i]))
+    # print(st)
+print(*result)
         
-            
-            
+
+# for i in range(1,N):
+#     # 입력받은 값이 현재 높이 값 보다 낮으면 pop
+#     if st[-1] < N_list[i] :
+#         5 < 7
+#         st.pop()
+#         # pop 후 stack 비어있을 경우 
+#         if len(st)==0:
+#             idx.append(0)
+#         else:
+#             idx.append(i)
+#     else: 
+#         idx.append(i)
+        
     
+#     st.append(N_list[i])
+    
+    
+# print(*idx)
+        
+
