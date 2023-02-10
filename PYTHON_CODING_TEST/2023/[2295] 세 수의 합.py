8348f4f9e1835d31ed36dff3816dacf5e3f60fab
GounChoi <61350052+gwcat0506@users.noreply.github.com>
2023-02-10 23:50:23 +0900

    230210

--- PYTHON_CODING_TEST/2023/[2295] 세 수의 합.py
+++ PYTHON_CODING_TEST/2023/[2295] 세 수의 합.py
@@ -53,51 +53,59 @@
     
 
 # 조합 풀이2 
-import sys
-from itertools import combinations, permutations
+# import sys
+# from itertools import combinations, permutations
 
+# input = sys.stdin.readline
+
+# n = int(input())
+
+# n_list = []
+# for _ in range(n):
+#     u = int(input())
+#     n_list.append(u)
+
+# # n_list = [1,2,3,5,6,10,18]
+# n_list.sort()
+
+
+# import heapq
+# # result를 heap으로 구성
+# heap=[]
+# # 정렬 후 이진탐색 하기 
+# for x in range(3,len(n_list)):
+#     l = []
+#     combis = list(combinations(n_list[:x], 3))
+#     for combi in combis:
+#         l.append(sum(combi))
+    
+#     l.sort()
+#     # print('l',l)
+    
+#     # x가 l에 있는지 확인
+#     start = 0
+#     end = len(l)-1
+    
+#     while start<=end:
+#         mid = (start+end)//2
+        
+#         if l[mid]==n_list[x]:
+#             # 최대 힙
+#             heapq.heappush(heap,[-n_list[x],n_list[x]])
+#             break
+#         elif l[mid] < n_list[x]:
+#             start = mid+1
+#         else:
+#             end = mid -1
+
+# # print(heap)
+# print(heapq.heappop(heap)[1])
+ 
+import sys
 input = sys.stdin.readline
 
 n = int(input())
 
-n_list = []
-for _ in range(n):
-    u = int(input())
-    n_list.append(u)
+u = [int(input()) for _ in range(n)]
 
-# n_list = [1,2,3,5,6,10,18]
-n_list.sort()
-
-
-import heapq
-# result를 heap으로 구성
-heap=[]
-# 정렬 후 이진탐색 하기 
-for x in range(3,len(n_list)):
-    l = []
-    combis = list(combinations(n_list[:x], 3))
-    for combi in combis:
-        l.append(sum(combi))
-    
-    l.sort()
-    # print('l',l)
-    
-    # x가 l에 있는지 확인
-    start = 0
-    end = len(l)-1
-    
-    while start<=end:
-        mid = (start+end)//2
-        
-        if l[mid]==n_list[x]:
-            # 최대 힙
-            heapq.heappush(heap,[-n_list[x],n_list[x]])
-            break
-        elif l[mid] < n_list[x]:
-            start = mid+1
-        else:
-            end = mid -1
-
-# print(heap)
-print(heapq.heappop(heap)[1])
 
--- PYTHON_CODING_TEST/2023/[2470] 두 용액.py
+++ PYTHON_CODING_TEST/2023/[2470] 두 용액.py
@@ -1,4 +1,5 @@
 
+# 
 import sys
 
 N = int(sys.stdin.readline())
@@ -7,8 +8,34 @@ N_list = list(map(int,sys.stdin.readline().split()))
 # 이진 탐색을 위한 sort
 N_list.sort()
 
-start = N_list[0]
-end = N_list[-1]*2
- 
- 
-# 모르겠다.... ㅠㅠ
+p1 = 0
+p2 = len(N_list)-1
+
+# 모두 산성 용액으로 이뤄졌을 경우 
+if N_list[0]>=0 and N_list[1]>=0:
+    print(N_list[0],N_list[1])
+# 모두 알칼리성 용액만으로 이뤄졌을 경우 
+elif N_list[-1]<=0 and N_list[-2]<=0:
+    print(N_list[-2],N_list[-1])
+    
+else:
+    # 초기화
+    result = [N_list[p1],N_list[p2],abs(N_list[p1]+N_list[p2])]
+
+    while p1<p2:
+        # print(p1,p2)
+        # 만약 포인터의 합이 더 작으면 result 바꿔줌
+        sum = N_list[p1] + N_list[p2]
+        if abs(sum) < result[2]:
+            result[0] = N_list[p1]
+            result[1] = N_list[p2]
+            result[2] = abs(sum)
+            if sum==0:
+                break
+            
+        if sum<0:
+            p1+=1
+        else:
+            p2-=1
+        
+    print(result[0],result[1])
