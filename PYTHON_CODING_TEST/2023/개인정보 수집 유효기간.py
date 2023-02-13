0303e5266e6863adbc31084acf109784f38f1418
GounChoi <61350052+gwcat0506@users.noreply.github.com>
2023-02-13 14:22:27 +0900

    230213

--- PYTHON_CODING_TEST/2023/개인정보 수집 유효기간.py
+++ PYTHON_CODING_TEST/2023/개인정보 수집 유효기간.py
@@ -1,4 +1,4 @@
-https://school.programmers.co.kr/learn/courses/30/lessons/150370
+# https://school.programmers.co.kr/learn/courses/30/lessons/150370
 
 def solution(today, terms, privacies):
     dic ={}
