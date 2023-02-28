a5e1dd3ca51d42447fb26e32a7ab1d45f1526485
GounChoi <61350052+gwcat0506@users.noreply.github.com>
2023-02-28 23:56:53 +0900

    230228

--- SQL_CODING_TEST/2023/최대값 구하기.sql
+++
@@ -1,5 +0,0 @@
--- https://school.programmers.co.kr/learn/courses/30/lessons/151136?language=mysql
-
-SELECT floor(avg(DAILY_FEE)) as AVERAGE_FEE
-FROM CAR_RENTAL_COMPANY_CAR
-WHERE CAR_TYPE = 'SUV';
