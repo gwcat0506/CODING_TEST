8b1da001960a6c3b499a7b32930c91c0c6fd90fd
gwcat0506 <61350052+gwcat0506@users.noreply.github.com>
2023-04-10 23:47:21 +0900

    230410

---
+++ SQL_CODING_TEST/2023/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,12 @@
+-- oracle
+-- https://school.programmers.co.kr/learn/courses/30/lessons/164672
+
+SELECT BOARD_ID, WRITER_ID, TITLE, PRICE, 
+CASE STATUS WHEN 'SALE' THEN '판매중'
+            WHEN 'RESERVED' THEN '예약중'
+            WHEN 'DONE' THEN '거래완료' END AS STATUS
+FROM USED_GOODS_BOARD
+WHERE TO_CHAR(CREATED_DATE,'YYYY-MM-DD') = '2022-10-05'
+ORDER BY BOARD_ID DESC
+
+-- CASE 쓰는법...
