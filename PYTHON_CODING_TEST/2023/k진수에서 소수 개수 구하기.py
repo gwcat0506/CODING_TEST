3bbca1f9fedc9fba33a4b2c40f40e858d08a5964
gwcat0506 <61350052+gwcat0506@users.noreply.github.com>
2023-04-14 23:59:38 +0900

    230414

--- PYTHON_CODING_TEST/2023/k진수에서 소수 개수 구하기.py
+++ PYTHON_CODING_TEST/2023/k진수에서 소수 개수 구하기.py
@@ -1,27 +1,40 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/92335
 
-def get_ginsu(n,k):
-    ginsu = ''
-    while n:
-        ginsu+=str(n%k)
-        n = n//k
-    return ginsu[::-1]
 
-#     소수 확인하기
-def sosu(num):
-#     num의 제곱근 까지만 나눠지는 수가 있는지 확인하면 됨 -> 없으면 소수 !
+def is_sosu(num):
+
+    if num==1:
+        return False 
 
     for i in range(2,int(num**(1/2))+1):
-        if num%i==0:
-            return False
+        if num%i == 0:
+            return False  
     return True
+        
+def make_jinsu(n,k):
+    jinsu = ''
     
-def solution(n, k):
-    result = 0
-    ginsu = get_ginsu(n,k)
-    print(ginsu.split('0'))
-    for gs in ginsu.split('0'):
-        if (gs != '') and (int(gs) > 1) and (sosu(int(gs))):
-            result+=1
+    while n:
+        if n == 0:
+            break
+        jinsu += str(n%k)
+        n = n//k
+#         문자열 뒤집기, 최종 k진수 만들기 성공(jinsu)
+    jinsu = jinsu[::-1]
+    return jinsu
 
-    return result
+def solution(n, k):
+    ans = 0
+    jinsu = make_jinsu(n,k)
+    
+# 0으로 split한 리스트
+    for i in jinsu.split('0'):
+        if i != '':
+#            빈칸이 아니면 소수인지 확인
+            if is_sosu(int(i)):
+                ans+=1
+        # print(i)
+          
+    return ans
+
+  
