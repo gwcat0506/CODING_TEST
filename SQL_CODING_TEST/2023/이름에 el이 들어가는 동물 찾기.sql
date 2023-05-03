f3d07f43ec4c41b5f02f41b638ec5ac55b80b8cc
gwcat0506 <61350052+gwcat0506@users.noreply.github.com>
2023-05-03 23:58:56 +0900

    230503

---
+++ SQL_CODING_TEST/2023/이름에 el이 들어가는 동물 찾기.sql
@@ -0,0 +1,6 @@
+-- oracle
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59047
+SELECT ANIMAL_ID,NAME
+FROM ANIMAL_INS
+WHERE LOWER(NAME) LIKE '%el%' AND ANIMAL_TYPE LIKE 'Dog'
+ORDER BY NAME;
